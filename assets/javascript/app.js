6b2e000241b42b24d9a7a539cc1fd662c45f67b8
Lucy M <lmcguigan@users.noreply.github.com>
2018-06-30 14:46:43 -0500

    add js

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var triviaItems = [];
+var question1 = {
+    questiontext: "",
+    answers: [{
+        answertext: "Answer",
+        isCorrect: true
+    }, {
+        answertext: "Answer",
+        isCorrect: false
+    }, {
+        answerText: "Answer",
+        isCorrect: false
+    }, {
+        answerText: "Answer",
+        isCorrect: false
+    }]
+}
+console.log(question1.answers[0].answertext);
